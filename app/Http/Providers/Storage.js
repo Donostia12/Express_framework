24d1c37b1eae8c1d94cee9eef21a5d588c9aeb16
Donostia12 <suncry7890@gmail.com>
2025-01-13 12:49:34 +0800

    storage

--- app/Http/Providers/Storage.js
+++ app/Http/Providers/Storage.js
@@ -3,7 +3,7 @@ const path = require("path");
 
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
-    cb(null, "storage/");
+    cb(null, "storage/images");
   },
   filename: (req, file, cb) => {
     cb(null, Date.now() + path.extname(file.originalname));
